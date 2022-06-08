7d42792afafbdff88104fc19317c56ab83da58da
Stan7b <coolgamer12611@gmail.com>
2022-06-08 12:53:54 +0300

    Lesson 2.XML-based Configuration

--- src/main/java/com/stan/spring/ApplicationRunner.java
+++ src/main/java/com/stan/spring/ApplicationRunner.java
@@ -5,23 +5,12 @@ import com.stan.spring.database.repository.CompanyRepository;
 import com.stan.spring.database.repository.UserRepository;
 import com.stan.spring.ioc.Container;
 import com.stan.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var container = new Container();
-
-
-//        var connectionpool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-        var userService = container.get(UserService.class);
-
-
-
-
-//        var connectionpool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionpool);
-//        var companyRepository = new CompanyRepository(connectionpool);
-//        var userService = new UserService(userRepository, companyRepository);
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        var connectionPool = context.getBean("p2",ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
