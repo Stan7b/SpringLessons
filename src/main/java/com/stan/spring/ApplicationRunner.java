7921d2ae3525bf889ee4b37ea2fcdcca73061224
Stan7b <coolgamer12611@gmail.com>
2022-06-08 20:27:15 +0300

    Lesson 6.LssonSix.LifecycleCallbacks.BeanScopes

--- src/main/java/com/stan/spring/ApplicationRunner.java
+++ src/main/java/com/stan/spring/ApplicationRunner.java
@@ -9,11 +9,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var connectionPool = context.getBean("p2",ConnectionPool.class);
-        System.out.println(connectionPool);
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
+            var connectionPool = context.getBean("p2", ConnectionPool.class);
+            System.out.println(connectionPool);
+
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println(companyRepository);
+        }
     }
 }
