8359ec2a3ccc09b9080aca55c084ecfb34a1a517
Stan7b <coolgamer12611@gmail.com>
2022-06-09 15:01:31 +0300

    Lesson 6.LssonSix.LifecycleCallbacks.BeanScopes

--- src/main/java/com/stan/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/stan/spring/database/pool/ConnectionPool.java
@@ -2,6 +2,8 @@ package com.stan.spring.database.pool;
 
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +27,7 @@ public class ConnectionPool implements InitializingBean {
         this.properties = properties;
     }
 
+    @PostConstruct
     private void init() {
         System.out.println("Init connection pool");
     }
@@ -33,7 +36,7 @@ public class ConnectionPool implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println("Properties set");
     }
-
+    @PreDestroy
     private void destroy() {
         System.out.println("Clean connection pool");
     }
