ce388689ce49c2cf7d599594e7a97bc5cc287ba5
Stan7b <coolgamer12611@gmail.com>
2022-06-13 15:52:05 +0300

    Lesson 10.Custom BeanPostProcessor.Unit-1

--- src/main/java/com/stan/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/stan/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,11 @@
 package com.stan.spring.database.repository;
 
+import com.stan.spring.bpp.InjectBean;
 import com.stan.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
+    @InjectBean
+    private  ConnectionPool connectionPool;
 
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool){
-        return new CompanyRepository(connectionPool);
-    }
 }
