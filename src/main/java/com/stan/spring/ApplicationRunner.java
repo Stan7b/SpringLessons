13bdb3dda62d4ed6e7e7738e74c71f0d62b96827
Stan7b <coolgamer12611@gmail.com>
2022-06-14 17:24:11 +0300

    Lesson 17.Java-based Configuration

--- src/main/java/com/stan/spring/ApplicationRunner.java
+++ src/main/java/com/stan/spring/ApplicationRunner.java
@@ -1,16 +1,18 @@
 package com.stan.spring;
 
+import com.stan.spring.config.ApplicationConfiguration;
 import com.stan.spring.database.pool.ConnectionPool;
 import com.stan.spring.database.repository.CompanyRepository;
 import com.stan.spring.database.repository.CrudRepository;
 import com.stan.spring.database.repository.UserRepository;
 import com.stan.spring.ioc.Container;
 import com.stan.spring.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
 
             var connectionPool = context.getBean("pool1", ConnectionPool.class);
             System.out.println(connectionPool);
---
+++ src/main/java/com/stan/spring/config/ApplicationConfiguration.java
@@ -0,0 +1,26 @@
+package com.stan.spring.config;
+
+
+import com.stan.spring.database.repository.CrudRepository;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.stan.spring",
+   useDefaultFilters = false,
+        includeFilters = {
+            @ComponentScan.Filter(type = FilterType.ANNOTATION,value = Component.class),
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,value = CrudRepository.class),
+            @ComponentScan.Filter(type = FilterType.REGEX,pattern = "com\\..+Repository")
+        }
+)
+public class ApplicationConfiguration {
+
+
+
+
+}
