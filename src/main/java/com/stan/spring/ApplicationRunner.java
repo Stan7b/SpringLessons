168663cb6067198799f7500b683a09d590fdac6d
Stan7b <coolgamer12611@gmail.com>
2022-06-17 14:20:45 +0300

    Lesson 23.Spring-boot

--- src/main/java/com/stan/spring/ApplicationRunner.java
+++ src/main/java/com/stan/spring/ApplicationRunner.java
@@ -8,21 +8,14 @@ import com.stan.spring.database.repository.UserRepository;
 import com.stan.spring.ioc.Container;
 import com.stan.spring.service.CompanyService;
 import com.stan.spring.service.UserService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web","prod");
-            context.refresh();
-
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean( CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class,args);
     }
 }
