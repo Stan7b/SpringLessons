e9bb2bb4a3dec14709561b08868e23843c0dda8a
Stan7b <coolgamer12611@gmail.com>
2022-06-18 18:37:27 +0300

    Lesson 34.Context caching

---
+++ src/test/java/com/stan/spring/service/integration/service/UserServiceIT.java
@@ -0,0 +1,35 @@
+package com.stan.spring.service.integration.service;
+
+
+import com.stan.spring.database.pool.ConnectionPool;
+import com.stan.spring.service.UserService;
+
+import com.stan.spring.service.integration.anotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.annotation.DirtiesContext;
+
+
+@IT
+@RequiredArgsConstructor
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserServiceIT {
+
+    private final UserService userService;
+    private final ConnectionPool pool1;
+
+    @Test
+    void test() {
+        System.out.println();
+    }
+
+    @Test
+    void test2() {
+        System.out.println();
+    }
+}
