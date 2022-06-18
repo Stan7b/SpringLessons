1d6e77947adeef95c7d757323d26f7f80b67a72e
Stan7b <coolgamer12611@gmail.com>
2022-06-18 17:53:50 +0300

    Lesson 33.Integration tests.Part3

--- src/test/java/com/stan/spring/service/integration/service/CompanyServiceIT.java
+++ src/test/java/com/stan/spring/service/integration/service/CompanyServiceIT.java
@@ -2,16 +2,21 @@ package com.stan.spring.service.integration.service;
 
 
 import com.stan.spring.ApplicationRunner;
+import com.stan.spring.config.DatabaseProperties;
 import com.stan.spring.database.entity.Company;
 import com.stan.spring.dto.CompanyReadDto;
 import com.stan.spring.listener.entity.EntityEvent;
 import com.stan.spring.service.CompanyService;
+import com.stan.spring.service.integration.anotation.IT;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -24,12 +29,13 @@ import static org.mockito.Mockito.*;
 
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
-    @Autowired
-    private CompanyService companyService;
 
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
     private static final Integer COMPANY_ID=1;
 
 
