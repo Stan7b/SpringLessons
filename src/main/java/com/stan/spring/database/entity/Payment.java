04089bd48e3a6b9e0b4958daf18b97a3c55e5795
Stan7b <coolgamer12611@gmail.com>
2022-06-29 19:24:55 +0300

    Lesson 36.Hibernate Entities

---
+++ src/main/java/com/stan/spring/database/entity/Payment.java
@@ -0,0 +1,41 @@
+package com.stan.spring.database.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "payment")
+public class Payment implements BaseEntity<Long>{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
