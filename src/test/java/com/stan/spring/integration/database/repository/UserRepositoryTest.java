28bde72f4d60e48a68702c2db1386d5ba7b90085
Stan7b <coolgamer12611@gmail.com>
2022-08-04 17:09:43 +0300

    Lesson42.@Query

---
+++ src/test/java/com/stan/spring/integration/database/repository/UserRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.stan.spring.integration.database.repository;
+
+import com.stan.spring.database.entity.User;
+import com.stan.spring.database.repository.UserRepository;
+import com.stan.spring.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+@IT
+@RequiredArgsConstructor
+class UserRepositoryTest {
+    private final UserRepository userRepository;
+
+    @Test
+    void checkQueries(){
+        var users = userRepository.findAllBy("a","ov");
+        Assertions.assertThat(users).hasSize(3);
+        System.out.println(users);
+    }
+
+
+
+}
